d3cadc8f1422cbd7e669a55dc94f70f1bbf2b6da
Yogita Negi <ynegi6@gmail.com>
2023-03-07 22:59:56 +0000

    Added files under components and added content in Navbarfile

---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        {/* menu */}
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li>Resume</li>
+            </ul>
+        </div>
+        {/* Hamburger */}
+        <div>
+            <FaBars />
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
