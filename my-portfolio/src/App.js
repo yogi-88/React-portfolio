6fced148287d331844bcee517380f2f778c0c4d4
Yogita Negi <ynegi6@gmail.com>
2023-03-08 00:58:51 +0000

    Deleted all files as tailwind not working, restarting work

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,21 +1,11 @@
-import React from "react";
-import Header from "./components/Header";
-import project from "./components/project";
-import Contact from "./Pages/Contact";
-import Homepage from "./Pages/Homepage";
-import Projects from "./Pages/Projects";
-export default function App() {
-  return (
-    <main>
-      <Header />
-      <Homepage />
-      <project />
-      <Projects />
-      <Contact />
-      
 
-    </main>
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Hello</h1>
+    </div>
   );
 }
 
-
+export default App;
