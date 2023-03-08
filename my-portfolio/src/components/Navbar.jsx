434a4013abbe3669262008cae74017ded602708c
Yogita Negi <ynegi6@gmail.com>
2023-03-08 01:35:02 +0000

    elements added to Navbar alongwith media query as per tailwindcss

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React , {useState} from 'react'
+import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>Navbar</div>
+      <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <h1>Yogita Negi</h1>
+        </div>
+          {/* menu */}
+
+          <ul className='hidden md:flex'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Skills</li>
+              <li>Projects</li>
+              <li>Contact</li>
+          </ul>
+
+          {/* hamburger*/}
+          <div className='md:hidden'>
+              <FaBars />
+          </div>
+
+          {/* mobile menu */}
+          <ul className='hidden'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Skills</li>
+              <li>Projects</li>
+              <li>Contact</li>
+          </ul>
+      </div>
   )
 }
 
