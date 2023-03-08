6bb00437986c14c24ec0c42941e7c1e85aca91bf
Yogita Negi <ynegi6@gmail.com>
2023-03-08 01:56:44 +0000

    Set the toggleable mobile menu on hamburger menu icon

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import React , {useState} from 'react'
 import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
+    {/* declares a state variable 'nav' that represents whether the navigation bar is open or close. and function 'setnav' that can be used to update the 'nav' variable */}
+    const [nav, setNav] = useState(false) 
+    const handleClick = () => setNav(!nav)
   return (
       <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -18,12 +21,12 @@ const Navbar = () => {
           </ul>
 
           {/* hamburger*/}
-          <div className='md:hidden z-10'>
+          <div onClick={handleClick} className='md:hidden z-10'>
               <FaBars />
           </div>
 
           {/* mobile menu */}
-          <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] text-gray-300 flex flex-col justify-center items-center'>
+          <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] text-gray-300 flex flex-col justify-center items-center'}>
               <li className='py-6' text-4xl>About</li>
               <li className='py-6' text-4xl>Skills</li>
               <li className='py-6' text-4xl>Home</li>
