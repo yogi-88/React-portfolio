b9ffcd4a3fa7831fddc6de8c6970aaf59842d368
Yogita Negi <ynegi6@gmail.com>
2023-03-08 01:45:52 +0000

    Added mediaquery for hamnurger and mobile section on navgar as per tailwindcss

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -18,17 +18,17 @@ const Navbar = () => {
           </ul>
 
           {/* hamburger*/}
-          <div className='md:hidden'>
+          <div className='md:hidden z-10'>
               <FaBars />
           </div>
 
           {/* mobile menu */}
-          <ul className='hidden'>
-              <li>Home</li>
-              <li>About</li>
-              <li>Skills</li>
-              <li>Projects</li>
-              <li>Contact</li>
+          <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] text-gray-300 flex flex-col justify-center items-center'>
+              <li className='py-6' text-4xl>About</li>
+              <li className='py-6' text-4xl>Skills</li>
+              <li className='py-6' text-4xl>Home</li>
+              <li className='py-6' text-4xl>Projects</li>
+              <li className='py-6' text-4xl>Contact</li>
           </ul>
       </div>
   )
