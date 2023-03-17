badbcd4dfec67c9c4b4a0dd00cb79b152d065550
rajeshwar singh bisht <ynegi6@gmail.com>
2023-03-17 23:48:18 +0000

    Added icons to footer section and linked to my social accounts

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -7,62 +8,76 @@ function Footer() {
         <div className="absolute bottom-0 left-0 right-0 z-10">
           <div
             className="absolute bottom-0 left-0 right-0 h-10 bg-no-repeat bg-cover"
-            style={{ backgroundImage: `url('https://i.ibb.co/wQZVxxk/wave.png')` }}
+            style={{
+              backgroundImage: `url('https://i.ibb.co/wQZVxxk/wave.png')`,
+            }}
           />
         </div>
         <div className="relative z-20">
           <ul className="flex justify-center items-center flex-wrap my-5">
             <li className="mr-4">
-              <a href="#" className="text-white text-2xl hover:translate-y-1 transition-all">
-                <ion-icon name="logo-facebook" />
+              <a href="https://github.com/yogi-88">
+                <FaGithub />
               </a>
             </li>
             <li className="mr-4">
-              <a href="#" className="text-white text-2xl hover:translate-y-1 transition-all">
-                <ion-icon name="logo-twitter" />
+              <a href="https://www.linkedin.com/in/yogita-negi-0aa79121/">
+                <FaLinkedin />
               </a>
             </li>
             <li className="mr-4">
-              <a href="#" className="text-white text-2xl hover:translate-y-1 transition-all">
-                <ion-icon name="logo-linkedin" />
-              </a>
-            </li>
-            <li className="mr-4">
-              <a href="#" className="text-white text-2xl hover:translate-y-1 transition-all">
-                <ion-icon name="logo-instagram" />
+              <a href="https://twitter.com/home">
+                <FaTwitter />
               </a>
             </li>
           </ul>
           <ul className="flex justify-center items-center flex-wrap my-5">
             <li className="mx-2">
-              <a href="#" className="text-white text-lg opacity-75 hover:opacity-100 transition-all">
+              <a
+                href="#"
+                className="text-white text-lg opacity-75 hover:opacity-100 transition-all"
+              >
                 Home
               </a>
             </li>
             <li className="mx-2">
-              <a href="#" className="text-white text-lg opacity-75 hover:opacity-100 transition-all">
+              <a
+                href="#"
+                className="text-white text-lg opacity-75 hover:opacity-100 transition-all"
+              >
                 About
               </a>
             </li>
             <li className="mx-2">
-              <a href="#" className="text-white text-lg opacity-75 hover:opacity-100 transition-all">
+              <a
+                href="#"
+                className="text-white text-lg opacity-75 hover:opacity-100 transition-all"
+              >
                 Services
               </a>
             </li>
             <li className="mx-2">
-              <a href="#" className="text-white text-lg opacity-75 hover:opacity-100 transition-all">
+              <a
+                href="#"
+                className="text-white text-lg opacity-75 hover:opacity-100 transition-all"
+              >
                 Team
               </a>
             </li>
             <li className="mx-2">
-              <a href="#" className="text-white text-lg opacity-75 hover:opacity-100 transition-all">
+              <a
+                href="#"
+                className="text-white text-lg opacity-75 hover:opacity-100 transition-all"
+              >
                 Contact
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <p className="text-white text-sm my-3">&copy;2023 Yogita Negi | All Rights Reserved</p>
+      <p className="text-white text-sm my-3">
+        &copy;2023 Yogita Negi | All Rights Reserved
+      </p>
     </footer>
   );
 }
