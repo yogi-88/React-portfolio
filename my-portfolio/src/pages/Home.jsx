b47af8a8a50397524e558fa310d9c1ffec1aa0ff
rajeshwar singh bisht <ynegi6@gmail.com>
2023-03-17 23:30:12 +0000

    Changed view work button to download resume on home page

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
               <p className='text-[#8892b0] py-4 max-w-[700px]'>Let's work together to create a website that meets your unique needs and exceeds your expectations.</p>
               <div>
                   <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                      View Work
+                      Download Resume
                       <span className='hover:rotate-90 duration-300'>
                           <HiArrowNarrowRight className='ml-3' />
 
